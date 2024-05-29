54ad6007f4a6b5417ddfea3edecb623daa165e6d
Martin Andersen <msandersen@gmail.com>
2024-05-29 14:58:22 -0400

    Added county metrics

---
+++ Datasets/make_clean_data.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+
+chr_data<-read_csv("https://www.countyhealthrankings.org/sites/default/files/media/document/analytic_data2024.csv")
+
+chr_data<-
+  chr_data|>
+  slice(-1)|>
+  rename(fips_code=`5-digit FIPS Code`)|>
+  filter(`County FIPS Code`!="000")|>
+  select(-contains("CI low"),-contains("CI high"))
+
+chr_data<-
+  chr_data|>
+  select(-`State FIPS Code`,-`County FIPS Code`,-`State Abbreviation`,-Name,-`Release Year`,-`County Clustered (Yes=1/No=0)`)
+
+chr_data<-
+  chr_data|>
+  select(-contains(" flag"),
+         -contains(" numerator"),
+         -contains(" denominator"))
+
+chr_data|>
+  pivot_longer(-fips_code,names_to = "measure")|>
+  mutate(value=as.numeric(value))|>
+  mutate(measure=str_remove(measure," raw value$"))|>
+  mutate(
+    measure=str_replace(measure,"(AIAN)","American Indian & Alaska Native"),
+    measure=str_replace(measure,"(NHOPI)","Native Hawaiian and Other Pacific Islander"),
+    # measure=str_replace(measure,"(AIAN)","(American Indian & Alaska Native)"),
+    # measure=str_replace(measure,"(AIAN)","(American Indian & Alaska Native)"),
+    # measure=str_replace(measure,"(AIAN)","(American Indian & Alaska Native)"),
+  )|>
+  write_csv("Datasets/chr_county_metrics_2024.csv")
